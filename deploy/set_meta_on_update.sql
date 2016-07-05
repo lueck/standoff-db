c170d465c7dc7325e9e068db2b530898355edbfd
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2016-07-05 17:04:26 +0200

    Fixed set_meta_on_update.

--- deploy/set_meta_on_update.sql
+++ deploy/set_meta_on_update.sql
@@ -8,7 +8,7 @@ CREATE FUNCTION standoff.set_meta_on_update()
     RETURNS TRIGGER AS $$
     BEGIN
     NEW.updated_at = current_timestamp;
-    NEW.updated_by = coalesce(NEW.created_by, current_user);
+    NEW.updated_by = current_user;
     NEW.created_at = OLD.created_at;
     NEW.created_by = OLD.created_by;
     RETURN NEW;
