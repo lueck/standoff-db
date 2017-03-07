070d2a07ce8804f91e70b5cd8d3d34e1481fa576
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-03-08 00:15:05 +0100

    corpus: more unit tests.

--- test/corpus.sql
+++ test/corpus.sql
@@ -1,8 +1,8 @@
 -- Start transaction and plan the tests
 BEGIN;
-SELECT plan(5);
+SELECT plan(6);
 
--- There is already a global in the table. 
+-- There is already a global corpus in the table.
 SELECT is(count(corpus_type)::integer, 1) FROM standoff.corpus WHERE corpus_type = 'global';
 
 -- There can only be one global corpus.
@@ -12,6 +12,17 @@ SELECT throws_ok('INSERT INTO standoff.corpus
 		 '23P01',
 		 'conflicting key value violates exclusion constraint "corpus_corpus_type_excl"');
 
+RESET ROLE;
+SET ROLE standoffuser;
+
+-- When we do this as a non super user the RLS policies prevent from
+-- adding a corpus_type <> 'collection'.
+SELECT throws_ok('INSERT INTO standoff.corpus 
+       			 (corpus_type, created_at, created_by, privilege)
+			 VALUES (''global'', current_timestamp, current_user, 365)',
+		 '42501',
+		 'new row violates row-level security policy for table "corpus"');
+
 -- There can be many user defined collections.
 SELECT lives_ok('INSERT INTO standoff.corpus 
        			(corpus_type, title, created_at, created_by, privilege)
@@ -20,6 +31,8 @@ SELECT lives_ok('INSERT INTO standoff.corpus
        			(corpus_type, title, created_at, created_by, privilege)
 			VALUES (''collection'', ''Arts'', current_timestamp, current_user, 365)');
 SELECT is(count(corpus_type)::integer, 2) FROM standoff.corpus WHERE corpus_type = 'collection';
+
+-- FIXME: Add more tests for RLS 
 
 -- Finish the tests and clean up.
 SELECT finish();
