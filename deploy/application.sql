26bfc51127caff8ae1ea48b9fd045c84b1f1cc7a
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-04-05 00:55:44 +0200

    application: insert basic application types

--- deploy/application.sql
+++ deploy/application.sql
@@ -9,6 +9,13 @@ CREATE TABLE standoff.application (
        description text,
        PRIMARY KEY (id));
 
+INSERT INTO standoff.application (id, description) VALUES
+       ('markup', 'Markup is used to decorate a range of a document.'),
+       ('relation', 'Relations are used interrelate markup.'),
+       ('attribute', 'Attributes are assigned to markup.')
+       ON CONFLICT DO NOTHING;
+
+
 GRANT SELECT ON TABLE standoff.application TO standoffuser, standoffeditor, standoffadmin;
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE standoff.application TO standoffadmin;
